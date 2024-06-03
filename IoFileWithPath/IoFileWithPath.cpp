dffd9f41d279d227b5d918eccb6a33ff704cd66d
Ekoramadhan13 <enugraha411@gmail.com>
2024-06-03 10:26:15 +0700

    menutup infile

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -27,6 +27,17 @@ int main()
 
 	infile.open(namafile + ".txt", ios:: in);
 
+	cout << endl << ">= membuka dan membaca file " << endl;
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << '\n';
+		}
+
+		infile.close();
+	}
+
 	
 }
 
