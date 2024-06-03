bc986e3bec18e5def4e63248214a0a8a339e0475
Ekoramadhan13 <enugraha411@gmail.com>
2024-06-03 10:36:00 +0700

    memasukann deklarasi menu

--- Exception/Exception.cpp
+++ Exception/Exception.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main()
 {
-	
+	cout << "awal program" << endl;
+	try {
+		array<int, 3> data = { 1,2,3 };
+		cout << data.at(5) << endl;
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+
+	cout << "baris program yang terakhir" << endl;
 }
 
---
+++ ManualException/ManualException.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "selamat belajar di prodi ti UMY" << endl;
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        cout << "default pengecualian diexsekusi" << endl;
+    }
+    
+}
+
+
